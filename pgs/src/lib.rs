514fb946fbf6307c06e17b9a7b7da6752b90ed53
diogo464 <diogo464@protonmail.com>
2025-01-02 23:17:49 +0000

    update

--- pgs/src/lib.rs
+++ pgs/src/lib.rs
@@ -1,14 +1,355 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::io::{Cursor, Read};
+
+pub mod wire;
+
+/// The graphics stream is made up of Functional Segments.
+/// The functional segments are:
+/// + Presentation Composition Segment (PCS)
+/// + Window Definition Segment (WDS)
+/// + Palette Definition Segment (PDS)
+/// + Object Definition Segment (ODS)
+/// + END of display set segment (END)
+///
+/// Screen Composition Segment: PCS
+/// Definition Segment: WDS, PDS, ODS, END
+
+#[derive(Debug, Clone, Copy)]
+pub enum CompositionState {
+    Normal,
+    AcquisitionPoint,
+    EpochStart,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Clone, Copy)]
+pub enum LastInSequenceFlag {
+    Last,
+    First,
+    FirstAndLast,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Clone)]
+pub enum Segment {
+    PCS(PCS),
+    WDS(WDS),
+    PDS(PDS),
+    ODS(ODS),
+    END(END),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct CompositionObjectCropping {
+    pub width: u16,
+    pub height: u16,
+    pub horizontal_position: u16,
+    pub vertical_position: u16,
+}
+
+#[derive(Debug, Clone)]
+pub struct CompositionObject {
+    pub object_id: u16,
+    pub window_id: u8,
+    pub horizontal_position: u16,
+    pub vertical_position: u16,
+    pub cropping: Option<CompositionObjectCropping>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Window {
+    /// uniquely identifies the window
+    pub window_id: u8,
+    /// range of 1 to (video width)-(window horizontal position)
+    pub width: u16,
+    /// range of 1 to (videoheight)-(windowVerticalposition)
+    pub height: u16,
+    /// top left pixel position
+    pub horizontal_position: u16,
+    /// top left pixel position
+    pub vertical_position: u16,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct PaletteEntry {
+    pub entry_id: u8,
+    pub luminance: u8,
+    pub color_diff_red: u8,
+    pub color_diff_blue: u8,
+    pub transparency: u8,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Header {
+    pub pts: u32,
+    pub dts: u32,
+}
+
+impl From<wire::SegmentHeader> for Header {
+    fn from(value: wire::SegmentHeader) -> Self {
+        Self {
+            pts: value.pts,
+            dts: value.dts,
+        }
+    }
+}
+
+/// Presentation Composition Segment
+#[derive(Debug, Clone)]
+pub struct PCS {
+    pub header: Header,
+    pub width: u16,
+    pub height: u16,
+    /// identifies this Graphics Update in the current Display Segment.
+    /// in range 0 - 15
+    pub composition_number: u16,
+    pub composition_state: CompositionState,
+    ///
+    pub palette_update: bool,
+    pub palette_id: u8,
+    pub composition_objects: Vec<CompositionObject>,
+}
+
+/// Window Definition Segment
+#[derive(Debug, Clone)]
+pub struct WDS {
+    pub header: Header,
+    pub windows: Vec<Window>,
+}
+
+/// Palette Definition Segment
+#[derive(Debug, Clone)]
+pub struct PDS {
+    pub header: Header,
+    /// uniquely identifies the palette
+    pub palette_id: u8,
+    /// version of the palette within the epoch
+    pub palette_version: u8,
+    pub entries: Vec<PaletteEntry>,
+}
+
+/// Object Definition Segment
+#[derive(Debug, Clone)]
+pub struct ODS {
+    pub header: Header,
+    /// uniquely identifies this object in the epoch.
+    /// an [`ODS`] segment with an object id that was already seen in the current epoch should
+    /// update the existing object.
+    pub object_id: u16,
+    pub object_version: u8,
+    pub last_in_sequence: LastInSequenceFlag,
+    /// the width for an object id should always be the same for a given epoch.
+    pub width: u16,
+    /// the height for an object id should always be the same for a given epoch.
+    pub height: u16,
+}
+
+/// END of display set segment
+#[derive(Debug, Clone)]
+pub struct END {
+    pub header: Header,
+}
+
+#[derive(Debug, Clone)]
+pub struct DisplaySet {
+    pub pcs: PCS,
+    pub wds: Vec<WDS>,
+    pub pds: Vec<PDS>,
+    pub ods: Vec<ODS>,
+    pub end: END,
+}
+
+pub fn decode_segment<R: Read>(mut reader: R) -> std::io::Result<Segment> {
+    use wire::Wire;
+
+    let header = wire::SegmentHeader::read(&mut reader)?;
+    if header.magic_number != wire::MAGIC_NUMBER {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "invalid header magic value",
+        ));
+    }
+
+    let mut buffer = Vec::new();
+    buffer.resize(header.segment_size as usize, 0);
+    reader.read_exact(&mut buffer)?;
+    let mut cursor = Cursor::new(&buffer);
+
+    match header.segment_type {
+        wire::SEGMENT_TYPE_PCS => {
+            let pcs = wire::SegmentPCS::read(&mut cursor)?;
+            let mut objects = Vec::with_capacity(pcs.number_of_composition_objects as usize);
+
+            for _ in 0..pcs.number_of_composition_objects {
+                let object = wire::CompositionObject::read(&mut cursor)?;
+                let cropping = match object.object_cropped_flag {
+                    wire::OBJECT_CROPPED_FLAG_OFF => None,
+                    wire::OBJECT_CROPPED_FLAG_FORCE => Some(CompositionObjectCropping {
+                        width: object.object_cropping_width,
+                        height: object.object_cropping_height,
+                        horizontal_position: object.object_cropping_horizontal_position,
+                        vertical_position: object.object_cropping_vertical_position,
+                    }),
+                    _ => {
+                        return Err(std::io::Error::new(
+                            std::io::ErrorKind::InvalidData,
+                            "invalid object cropped flag",
+                        ))
+                    }
+                };
+                objects.push(CompositionObject {
+                    object_id: object.object_id,
+                    window_id: object.window_id,
+                    horizontal_position: object.object_horizontal_position,
+                    vertical_position: object.object_vertical_position,
+                    cropping,
+                });
+            }
+
+            let palette_update = match pcs.palette_update_flag {
+                wire::PALETTE_UPDATE_FLAG_TRUE => true,
+                wire::PALETTE_UPDATE_FLAG_FALSE => false,
+                _ => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::InvalidData,
+                        "invalid palette update flag",
+                    ))
+                }
+            };
+
+            let composition_state = match pcs.composition_state {
+                wire::COMPOSITION_STATE_EPOCH_START => CompositionState::EpochStart,
+                wire::COMPOSITION_STATE_ACQUISITION_POINT => CompositionState::AcquisitionPoint,
+                wire::COMPOSITION_STATE_NORMAL => CompositionState::Normal,
+                _ => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::InvalidData,
+                        "invalid composition state",
+                    ))
+                }
+            };
+
+            Ok(Segment::PCS(PCS {
+                header: Header::from(header),
+                width: pcs.width,
+                height: pcs.height,
+                composition_number: pcs.composition_number,
+                composition_state,
+                palette_update,
+                palette_id: pcs.palette_id,
+                composition_objects: objects,
+            }))
+        }
+        wire::SEGMENT_TYPE_WDS => {
+            let wds = wire::SegmentWDS::read(&mut cursor)?;
+            let mut windows = Vec::with_capacity(wds.number_of_windows as usize);
+            for _ in 0..wds.number_of_windows {
+                let wnd = wire::Window::read(&mut cursor)?;
+                windows.push(Window {
+                    window_id: wnd.window_id,
+                    width: wnd.window_width,
+                    height: wnd.window_height,
+                    horizontal_position: wnd.window_horizontal_position,
+                    vertical_position: wnd.window_vertical_position,
+                });
+            }
+            Ok(Segment::WDS(WDS {
+                header: Header::from(header),
+                windows,
+            }))
+        }
+        wire::SEGMENT_TYPE_PDS => {
+            let pds = wire::SegmentPDS::read(&mut cursor)?;
+            let mut entries = Vec::new();
+            while cursor.position() < buffer.len() as u64 {
+                let entry = wire::PaletteEntry::read(&mut cursor)?;
+                entries.push(PaletteEntry {
+                    entry_id: entry.palette_entry_id,
+                    luminance: entry.luminance,
+                    color_diff_red: entry.color_diff_red,
+                    color_diff_blue: entry.color_diff_blue,
+                    transparency: entry.transparency,
+                });
+            }
+            Ok(Segment::PDS(PDS {
+                header: Header::from(header),
+                palette_id: pds.palette_id,
+                palette_version: pds.palette_version,
+                entries,
+            }))
+        }
+        wire::SEGMENT_TYPE_ODS => {
+            let ods = wire::SegmentODS::read(&mut cursor)?;
+            let mut data = vec![0u8; ods.object_data_length.saturating_sub(4) as usize];
+            cursor.read_exact(&mut data)?;
+
+            let flag = match ods.last_in_sequence_flag {
+                wire::LAST_IN_SEQUENCE_FLAG_FIRST_IN_SEQ => LastInSequenceFlag::First,
+                wire::LAST_IN_SEQUENCE_FLAG_LAST_IN_SEQ => LastInSequenceFlag::Last,
+                wire::LAST_IN_SEQUENCE_FLAG_FIRST_AND_LAST_IN_SEQ => {
+                    LastInSequenceFlag::FirstAndLast
+                }
+                _ => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::InvalidData,
+                        "invalid last in sequence flag",
+                    ))
+                }
+            };
+
+            Ok(Segment::ODS(ODS {
+                header: Header::from(header),
+                object_id: ods.object_id,
+                object_version: ods.object_version,
+                last_in_sequence: flag,
+                width: ods.width,
+                height: ods.height,
+            }))
+        }
+        wire::SEGMENT_TYPE_END => Ok(Segment::END(END {
+            header: Header::from(header),
+        })),
+        _ => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "invalid segment type",
+            ))
+        }
+    }
+}
+
+pub fn decode_display_set<R: Read>(mut reader: R) -> std::io::Result<DisplaySet> {
+    let pcs = match decode_segment(&mut reader)? {
+        Segment::PCS(pcs) => pcs,
+        _ => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "expected pcs as first segment in display set",
+            ))
+        }
+    };
+
+    let mut vwds = Vec::new();
+    let mut vpds = Vec::new();
+    let mut vods = Vec::new();
+
+    loop {
+        match decode_segment(&mut reader)? {
+            Segment::PCS(_) => {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "found PCS in the middle of display set",
+                ))
+            }
+            Segment::WDS(wds) => vwds.push(wds),
+            Segment::PDS(pds) => vpds.push(pds),
+            Segment::ODS(ods) => vods.push(ods),
+            Segment::END(end) => {
+                return Ok(DisplaySet {
+                    pcs,
+                    wds: vwds,
+                    pds: vpds,
+                    ods: vods,
+                    end,
+                })
+            }
+        }
     }
 }
---
+++ pgs/src/wire.rs
@@ -0,0 +1,354 @@
+use std::io::Read;
+
+// https://blog.thescorpius.com/index.php/2017/07/15/presentation-graphic-stream-sup-files-bluray-subtitle-format/
+
+pub const MAGIC_NUMBER: u16 = 0x5047; // b"PG"
+
+pub const SEGMENT_TYPE_PDS: u8 = 0x14;
+pub const SEGMENT_TYPE_ODS: u8 = 0x15;
+pub const SEGMENT_TYPE_PCS: u8 = 0x16;
+pub const SEGMENT_TYPE_WDS: u8 = 0x17;
+pub const SEGMENT_TYPE_END: u8 = 0x80;
+
+pub const FRAME_RATE: u8 = 0x10;
+
+pub const COMPOSITION_STATE_NORMAL: u8 = 0x00;
+pub const COMPOSITION_STATE_ACQUISITION_POINT: u8 = 0x40;
+pub const COMPOSITION_STATE_EPOCH_START: u8 = 0x80;
+
+pub const PALETTE_UPDATE_FLAG_FALSE: u8 = 0x00;
+pub const PALETTE_UPDATE_FLAG_TRUE: u8 = 0x80;
+
+pub const OBJECT_CROPPED_FLAG_OFF: u8 = 0x00;
+pub const OBJECT_CROPPED_FLAG_FORCE: u8 = 0x40;
+
+pub const LAST_IN_SEQUENCE_FLAG_LAST_IN_SEQ: u8 = 0x40;
+pub const LAST_IN_SEQUENCE_FLAG_FIRST_IN_SEQ: u8 = 0x80;
+pub const LAST_IN_SEQUENCE_FLAG_FIRST_AND_LAST_IN_SEQ: u8 =
+    LAST_IN_SEQUENCE_FLAG_FIRST_IN_SEQ | LAST_IN_SEQUENCE_FLAG_LAST_IN_SEQ;
+
+pub trait Wire: Sized {
+    fn read<R: Read>(reader: R) -> std::io::Result<Self>;
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub struct SegmentHeader {
+    pub magic_number: u16,
+    pub pts: u32,
+    pub dts: u32,
+    pub segment_type: u8,
+    pub segment_size: u16,
+}
+
+/// Presentation Composition Segment
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub struct SegmentPCS {
+    pub width: u16,
+    pub height: u16,
+    pub framerate: u8,
+    pub composition_number: u16,
+    pub composition_state: u8,
+    pub palette_update_flag: u8,
+    pub palette_id: u8,
+    pub number_of_composition_objects: u8,
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub struct CompositionObject {
+    pub object_id: u16,
+    pub window_id: u8,
+    pub object_cropped_flag: u8,
+    pub object_horizontal_position: u16,
+    pub object_vertical_position: u16,
+    pub object_cropping_horizontal_position: u16,
+    pub object_cropping_vertical_position: u16,
+    pub object_cropping_width: u16,
+    pub object_cropping_height: u16,
+}
+
+/// Window Definition Segment
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub struct SegmentWDS {
+    pub number_of_windows: u8,
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub struct Window {
+    pub window_id: u8,
+    pub window_horizontal_position: u16,
+    pub window_vertical_position: u16,
+    pub window_width: u16,
+    pub window_height: u16,
+}
+
+/// Palette Definition Segment
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub struct SegmentPDS {
+    pub palette_id: u8,
+    pub palette_version: u8,
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub struct PaletteEntry {
+    pub palette_entry_id: u8,
+    pub luminance: u8,
+    pub color_diff_red: u8,
+    pub color_diff_blue: u8,
+    pub transparency: u8,
+}
+
+/// Object Definition Segment
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+pub struct SegmentODS {
+    pub object_id: u16,
+    pub object_version: u8,
+    pub last_in_sequence_flag: u8,
+    pub object_data_length: u32,
+    pub width: u16,
+    pub height: u16,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ImageDataCode {
+    Pixels { color: u8, count: u16 },
+    EndOfLine,
+}
+
+struct ImageDataDecoder<'a> {
+    buf: &'a [u8],
+    offset: usize,
+}
+
+impl<'a> ImageDataDecoder<'a> {
+    fn new(buf: &'a [u8]) -> Self {
+        Self { buf, offset: 0 }
+    }
+}
+
+impl<'a> Iterator for ImageDataDecoder<'a> {
+    type Item = std::io::Result<ImageDataCode>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.offset == self.buf.len() {
+            return None;
+        }
+        let (code, n) = match decode_image_data_code(&self.buf[self.offset..]) {
+            Ok((code, n)) => (code, n),
+            Err(err) => return Some(Err(err)),
+        };
+        self.offset += n;
+        Some(Ok(code))
+    }
+}
+
+pub fn decode_image_data_code(buf: &[u8]) -> std::io::Result<(ImageDataCode, usize)> {
+    if buf.is_empty() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::UnexpectedEof,
+            "empty buffer",
+        ));
+    }
+
+    let v0 = buf[0];
+    if v0 > 0 {
+        return Ok((
+            ImageDataCode::Pixels {
+                color: v0,
+                count: 1,
+            },
+            1,
+        ));
+    }
+
+    if buf.len() < 2 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "not enough data",
+        ));
+    }
+
+    let v1 = buf[1];
+    if v1 >= 1 && v1 <= 63 {
+        return Ok((
+            ImageDataCode::Pixels {
+                color: 0,
+                count: u16::from(v1),
+            },
+            2,
+        ));
+    }
+
+    if v1 == 0 {
+        return Ok((ImageDataCode::EndOfLine, 2));
+    }
+
+    if buf.len() < 3 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "not enough data",
+        ));
+    }
+
+    let v2 = buf[2];
+    if v1 & 0b01000000 == 0b01000000 {
+        let v1 = u16::from(v1);
+        let v2 = u16::from(v2);
+        let n = (v1 & 0b00111111) << 8 | v2;
+        let n = n.saturating_sub(1);
+        return Ok((ImageDataCode::Pixels { color: 0, count: n }, 3));
+    }
+
+    if v1 & 0b10000000 == 0b10000000 {
+        let n = u16::from(v1 & 0b00111111);
+        let c = v2;
+        return Ok((ImageDataCode::Pixels { color: c, count: n }, 3));
+    }
+
+    if buf.len() < 4 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "not enough data",
+        ));
+    }
+
+    let v3 = buf[3];
+    if v1 & 0b11000000 == 0b11000000 {
+        let v1 = u16::from(v1);
+        let v2 = u16::from(v2);
+        let c = v3;
+        let n = (v1 & 0b00111111) << 8 | v2;
+        return Ok((ImageDataCode::Pixels { color: c, count: n }, 4));
+    }
+
+    return Err(std::io::Error::new(
+        std::io::ErrorKind::InvalidData,
+        "invalid rle data",
+    ));
+}
+
+pub fn decode_image_data(buf: &[u8]) -> impl Iterator<Item = std::io::Result<ImageDataCode>> + '_ {
+    ImageDataDecoder::new(buf)
+}
+
+fn read_u8<R: Read>(reader: &mut R) -> std::io::Result<u8> {
+    let mut buf = [0u8; 1];
+    reader.read_exact(&mut buf)?;
+    Ok(buf[0])
+}
+
+fn read_u16<R: Read>(reader: &mut R) -> std::io::Result<u16> {
+    let mut buf = [0u8; 2];
+    reader.read_exact(&mut buf)?;
+    Ok(u16::from_be_bytes(buf))
+}
+
+fn read_u24<R: Read>(reader: &mut R) -> std::io::Result<u32> {
+    let mut buf = [0u8; 4];
+    reader.read_exact(&mut buf[1..])?;
+    Ok(u32::from_be_bytes(buf))
+}
+
+fn read_u32<R: Read>(reader: &mut R) -> std::io::Result<u32> {
+    let mut buf = [0u8; 4];
+    reader.read_exact(&mut buf)?;
+    Ok(u32::from_be_bytes(buf))
+}
+
+impl Wire for SegmentHeader {
+    fn read<R: Read>(mut reader: R) -> std::io::Result<Self> {
+        Ok(Self {
+            magic_number: read_u16(&mut reader)?,
+            pts: read_u32(&mut reader)?,
+            dts: read_u32(&mut reader)?,
+            segment_type: read_u8(&mut reader)?,
+            segment_size: read_u16(&mut reader)?,
+        })
+    }
+}
+
+impl Wire for SegmentPCS {
+    fn read<R: Read>(mut reader: R) -> std::io::Result<Self> {
+        Ok(Self {
+            width: read_u16(&mut reader)?,
+            height: read_u16(&mut reader)?,
+            framerate: read_u8(&mut reader)?,
+            composition_number: read_u16(&mut reader)?,
+            composition_state: read_u8(&mut reader)?,
+            palette_update_flag: read_u8(&mut reader)?,
+            palette_id: read_u8(&mut reader)?,
+            number_of_composition_objects: read_u8(&mut reader)?,
+        })
+    }
+}
+
+impl Wire for CompositionObject {
+    fn read<R: Read>(mut reader: R) -> std::io::Result<Self> {
+        let mut s = Self::default();
+        s.object_id = read_u16(&mut reader)?;
+        s.window_id = read_u8(&mut reader)?;
+        s.object_cropped_flag = read_u8(&mut reader)?;
+        s.object_horizontal_position = read_u16(&mut reader)?;
+        s.object_vertical_position = read_u16(&mut reader)?;
+        if s.object_cropped_flag == OBJECT_CROPPED_FLAG_FORCE {
+            s.object_cropping_horizontal_position = read_u16(&mut reader)?;
+            s.object_cropping_vertical_position = read_u16(&mut reader)?;
+            s.object_cropping_width = read_u16(&mut reader)?;
+            s.object_cropping_height = read_u16(&mut reader)?;
+        }
+        Ok(s)
+    }
+}
+
+impl Wire for SegmentWDS {
+    fn read<R: Read>(mut reader: R) -> std::io::Result<Self> {
+        Ok(Self {
+            number_of_windows: read_u8(&mut reader)?,
+        })
+    }
+}
+
+impl Wire for Window {
+    fn read<R: Read>(mut reader: R) -> std::io::Result<Self> {
+        Ok(Self {
+            window_id: read_u8(&mut reader)?,
+            window_horizontal_position: read_u16(&mut reader)?,
+            window_vertical_position: read_u16(&mut reader)?,
+            window_width: read_u16(&mut reader)?,
+            window_height: read_u16(&mut reader)?,
+        })
+    }
+}
+
+impl Wire for SegmentPDS {
+    fn read<R: Read>(mut reader: R) -> std::io::Result<Self> {
+        Ok(Self {
+            palette_id: read_u8(&mut reader)?,
+            palette_version: read_u8(&mut reader)?,
+        })
+    }
+}
+
+impl Wire for PaletteEntry {
+    fn read<R: Read>(mut reader: R) -> std::io::Result<Self> {
+        Ok(Self {
+            palette_entry_id: read_u8(&mut reader)?,
+            luminance: read_u8(&mut reader)?,
+            color_diff_red: read_u8(&mut reader)?,
+            color_diff_blue: read_u8(&mut reader)?,
+            transparency: read_u8(&mut reader)?,
+        })
+    }
+}
+
+impl Wire for SegmentODS {
+    fn read<R: Read>(mut reader: R) -> std::io::Result<Self> {
+        Ok(Self {
+            object_id: read_u16(&mut reader)?,
+            object_version: read_u8(&mut reader)?,
+            last_in_sequence_flag: read_u8(&mut reader)?,
+            object_data_length: read_u24(&mut reader)?,
+            width: read_u16(&mut reader)?,
+            height: read_u16(&mut reader)?,
+        })
+    }
+}
